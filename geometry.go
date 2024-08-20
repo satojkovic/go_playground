3578701fa467ba57c683bf97629f0db6bb3535b4
satojkovic <satojkovic@gmail.com>
2024-08-20 22:41:39 +0900

    Add receiver sample

---
+++ geometry.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct{ X, Y float64 }
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(p.Distance(q))
+
+	p.ScaleBy(2)
+	fmt.Println(p)
+}
